057be5cefbfdaf9b0a742b0695c5d27f5a4a2d2d
moahmed hussein <mh95568@gmail.com>
2023-05-06 03:55:17 +0300

    menu screen and start button in logo screen

--- app/src/main/java/com/example/onlinedoctor/LoginScreen.java
+++ app/src/main/java/com/example/onlinedoctor/LoginScreen.java
@@ -2,13 +2,33 @@ package com.example.onlinedoctor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginScreen extends AppCompatActivity {
-
+    Button login ;
+    TextView forgot, or ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+        login = findViewById(R.id.button_login);
+        forgot = findViewById(R.id.text_forgot);
+        or = findViewById(R.id.text_or);
+        login.setOnClickListener(v -> {
+            startActivity(new Intent(LoginScreen.this ,MenuScreen.class));
+            finish();
+        });
+        forgot.setOnClickListener(v ->{
+            startActivity(new Intent(LoginScreen.this ,MenuScreen.class));
+            finish();
+        });
+        or.setOnClickListener(v ->{
+            startActivity(new Intent(LoginScreen.this ,MenuScreen.class));
+            finish();
+        });
     }
 }
---
+++ app/src/main/java/com/example/onlinedoctor/MenuScreen.java
@@ -0,0 +1,36 @@
+package com.example.onlinedoctor;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MenuScreen extends AppCompatActivity {
+    CardView heart, skinCancer, pneumonia, malaria;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_screen);
+
+        heart = findViewById(R.id.cv_heart);
+        skinCancer = findViewById(R.id.cv_skinCancer);
+        pneumonia = findViewById(R.id.cv_pneumonia);
+        malaria = findViewById(R.id.cv_malaria);
+
+
+        heart.setOnClickListener(v -> {
+            Toast.makeText(this, "heart Screen", Toast.LENGTH_LONG).show();
+        });
+        skinCancer.setOnClickListener(v -> {
+            Toast.makeText(this, "Skin Cancer Screen", Toast.LENGTH_LONG).show();
+        });
+        pneumonia.setOnClickListener(v -> {
+            Toast.makeText(this, "pneumonia Screen", Toast.LENGTH_LONG).show();
+        });
+        malaria.setOnClickListener(v -> {
+            Toast.makeText(this, "malaria Screen", Toast.LENGTH_LONG).show();
+        });
+    }
+}
