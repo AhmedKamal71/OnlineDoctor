8b41be5db733632ca812e936495ee1c3832ef3e0
moahmed hussein <mh95568@gmail.com>
2023-05-19 05:32:43 +0300

    BreastCancer Model And Screen ui end

--- app/src/main/java/com/example/onlinedoctor/MenuScreen.java
+++ app/src/main/java/com/example/onlinedoctor/MenuScreen.java
@@ -7,8 +7,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.example.onlinedoctor.breast_cancer.BreastCancer;
+
 public class MenuScreen extends AppCompatActivity {
-    CardView heart, skinCancer, pneumonia, malaria;
+    CardView heart, skinCancer, pneumonia, malaria,breast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +21,7 @@ public class MenuScreen extends AppCompatActivity {
         skinCancer = findViewById(R.id.cv_skinCancer);
         pneumonia = findViewById(R.id.cv_pneumonia);
         malaria = findViewById(R.id.cv_malaria);
-
+        breast = findViewById(R.id.cv_breastCancer);
 
         heart.setOnClickListener(v -> {
             Toast.makeText(this, "heart Screen", Toast.LENGTH_LONG).show();
@@ -28,10 +30,13 @@ public class MenuScreen extends AppCompatActivity {
             startActivity(new Intent(MenuScreen.this , SkinDetectionScreen.class));
         });
         pneumonia.setOnClickListener(v -> {
-            Toast.makeText(this, "pneumonia Screen", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(MenuScreen.this , Pneumonia.class));
         });
         malaria.setOnClickListener(v -> {
             Toast.makeText(this, "malaria Screen", Toast.LENGTH_LONG).show();
+        });
+        breast.setOnClickListener(v -> {
+            startActivity(new Intent(MenuScreen.this , BreastCancer.class));
         });
     }
 }
