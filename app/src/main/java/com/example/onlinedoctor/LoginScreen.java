9eb9e5dfcb3a0a89c1bd623b3d67590f2a1e3300
moahmed hussein <mh95568@gmail.com>
2023-05-22 03:59:06 +0300

    liver and malaria screen and model final edit

--- app/src/main/java/com/example/onlinedoctor/LoginScreen.java
+++ app/src/main/java/com/example/onlinedoctor/LoginScreen.java
@@ -6,11 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginScreen extends AppCompatActivity {
-    Button login ;
-    TextView forgot, or ;
+    Button login;
+    TextView forgot, or;
+    EditText email;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,16 +22,19 @@ public class LoginScreen extends AppCompatActivity {
         login = findViewById(R.id.button_login);
         forgot = findViewById(R.id.text_forgot);
         or = findViewById(R.id.text_or);
+        email = findViewById(R.id.email);
         login.setOnClickListener(v -> {
-            startActivity(new Intent(LoginScreen.this ,MenuScreen.class));
+                Intent intent = new Intent(LoginScreen.this, MenuScreen.class);
+                intent.putExtra("name", email.getText().toString());
+                startActivity(intent);
+                finish();
+        });
+        forgot.setOnClickListener(v -> {
+            startActivity(new Intent(LoginScreen.this, MenuScreen.class));
             finish();
         });
-        forgot.setOnClickListener(v ->{
-            startActivity(new Intent(LoginScreen.this ,MenuScreen.class));
-            finish();
-        });
-        or.setOnClickListener(v ->{
-            startActivity(new Intent(LoginScreen.this ,MenuScreen.class));
+        or.setOnClickListener(v -> {
+            startActivity(new Intent(LoginScreen.this, MenuScreen.class));
             finish();
         });
     }
--- app/src/main/java/com/example/onlinedoctor/MenuScreen.java
+++ app/src/main/java/com/example/onlinedoctor/MenuScreen.java
@@ -5,24 +5,26 @@ import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.onlinedoctor.breast_cancer.BreastCancer;
 
 public class MenuScreen extends AppCompatActivity {
-    CardView heart, skinCancer, pneumonia, malaria,breast;
-
+    CardView heart, skinCancer, pneumonia, malaria,breast,liver;
+    TextView userName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_screen);
-
+        userName = findViewById(R.id.textView_name);
         heart = findViewById(R.id.cv_heart);
         skinCancer = findViewById(R.id.cv_skinCancer);
         pneumonia = findViewById(R.id.cv_pneumonia);
         malaria = findViewById(R.id.cv_malaria);
         breast = findViewById(R.id.cv_breastCancer);
-
+        liver = findViewById(R.id.cv_liver);
+        userName.setText("Welcome: "+getIntent().getStringExtra("name"));
         heart.setOnClickListener(v -> {
             startActivity(new Intent(MenuScreen.this , Heart.class));
         });
@@ -33,10 +35,16 @@ public class MenuScreen extends AppCompatActivity {
             startActivity(new Intent(MenuScreen.this , Pneumonia.class));
         });
         malaria.setOnClickListener(v -> {
-            Toast.makeText(this, "malaria Screen", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(MenuScreen.this , Malaria.class));
         });
         breast.setOnClickListener(v -> {
             startActivity(new Intent(MenuScreen.this , BreastCancer.class));
         });
+
+        liver.setOnClickListener(v ->
+        {
+            startActivity(new Intent(MenuScreen.this ,Liver.class));
+        });
+
     }
 }
